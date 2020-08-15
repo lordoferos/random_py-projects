cde252318cde89bc42151b7dd07f90124cead487
lordoferos <33201932+lordoferos@users.noreply.github.com>
2020-08-15 20:39:21 +0300

    Update spiralDraw.py

--- spiralDraw.py
+++ spiralDraw.py
@@ -2,9 +2,14 @@
 # spiralDraw.py - Draw using mouse
 
 import pyautogui, time
+
+# Pause for 10 seconds to allow you move to MS paint
+# And select the brush
 time.sleep(10)
+
 pyautogui.click() # click to put drawing program in focus
-distance = 200
+# set size
+distance = 400
 while distance > 0:
     pyautogui.dragRel(distance, 0, duration=0.2) # move right
     distance = distance - 5
